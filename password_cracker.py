1934b35993b745e8552c99b1b56ddad95a21572e
antosb <74680371+antosb@users.noreply.github.com>
2020-11-18 22:07:10 +0100

    Add files via upload

---
+++ password_cracker.py
@@ -0,0 +1,28 @@
+
+from random import *
+
+
+user_pass = input("Pon tu contraseña")
+
+
+password = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u','v', 
+            'w', 'x', 'y', 'z', '1','2', '2','4','5','6','7','8','9','10']
+
+
+guess = ""
+
+
+
+
+while (guess != user_pass):
+    guess = ""
+    
+    for letter in range(len(user_pass)):
+        guess_letter = password[randint(0, 25)]
+        guess = str(guess_letter) + str(guess)
+    
+    print(guess)
+    
+
+print("Tu contrsaeña es: ",guess)
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Craqueador de contraseñas en pyton 
+
+password cracker
+
+follow me on:
+
+-instagram- @__antoosb
+-twitter- @__antoosb
